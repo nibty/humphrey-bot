41158b0823fe77ad8eef97d021e2c60a476e1de5
Nicholas Pettas <npettas@gmail.com>
2023-06-20 14:56:52 -0700

    save

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -11,13 +11,12 @@ import xacro
 
 
 def generate_launch_description():
-
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('humphrey_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
 
     # Create a robot_state_publisher node
@@ -28,7 +27,6 @@ def generate_launch_description():
         output='screen',
         parameters=[params]
     )
-
 
     # Launch!
     return LaunchDescription([
