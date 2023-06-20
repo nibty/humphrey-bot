5955fe7feec1c7afd5d05b1f11118ddbba7d92f1
Nicholas Pettas <npettas@gmail.com>
2023-06-20 15:11:52 -0700

    save

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -3,10 +3,8 @@ import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-
-
-
     return LaunchDescription([
 
         Node(
@@ -15,9 +13,9 @@ def generate_launch_description():
             output='screen',
             namespace='camera',
             parameters=[{
-                'image_size': [640,480],
+                'image_size': [640, 480],
                 'time_per_frame': [1, 6],
                 'camera_frame_id': 'camera_link_optical'
-                }]
-    )
+            }]
+        )
     ])
